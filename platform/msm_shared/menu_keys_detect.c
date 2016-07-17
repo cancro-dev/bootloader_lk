f6f0e12549e58cae12aa00d4db6f1d425696ba24
sndnvaps <sndnvaps@gmail.com>
2016-07-17 13:36:12 +0800

    Fix build error for fb_display_menu  when ENABLE_FBCON_DISPLAY_MSG :=1 set in lk_inc.mk
    Signed-off-by: sndnvaps <sndnvaps@gmail.com>

--- platform/msm_shared/menu_keys_detect.c
+++ platform/msm_shared/menu_keys_detect.c
@@ -50,10 +50,10 @@
 
 static time_t before_time;
 
-extern int target_volume_up();
-extern uint32_t target_volume_down();
+extern uint32_t target_volume_up(void);
+extern uint32_t target_volume_down(void);
 extern void reboot_device(unsigned reboot_reason);
-extern void shutdown_device();
+extern void shutdown_device(void);
 
 typedef uint32_t (*keys_detect_func)(void);
 typedef void (*keys_action_func)(struct select_msg_info* msg_info);
